f3ac5c9ee5908a32dd6668f04f0f97d9631fd0bc
Rick Alves <henryworkdev.ti@gmail.com>
2023-04-07 20:58:53 -0300

    adicionando modelo fotografia

--- galeria/models.py
+++ galeria/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Fotografia(models.Model):
+    CATEGORIAS = [
+        ("ESTRELA", "Estrela"),
+        ("GALAXIA", "Galáxia"),
+        ("NEBULOSA", "Nebulosa"),
+        ("PLANETA", "Planeta"),
+    ]
+
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    legenda = models.CharField(max_length=200, null=False, blank=False)
+    categoria = models.CharField(max_length=50, choices=CATEGORIAS, default='')
+    descricao = models.TextField(null=False, blank=False)
+    foto = models.CharField(max_length=100, null=False, blank=False)
+
+    def __str__(self):
+        return f"Fotografia -> [{self.nome}]"
--- galeria/views.py
+++ galeria/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from galeria.models import Fotografia
 
 def index(request):
-    return render(request, 'galeria/index.html')
+    # dados = {
+    #     1: {"nome": "Nebulosa de Carina",
+    #         "legenda": "webbtelescope.org/ NASA / James Webb"},
+    #     2: {"nome": "Galaxia NGC 1079",
+    #         "legenda": "nasa.org/ NASA / Hobbe"}
+    # }
 
-def imagem(request):
-    return render(request, 'galeria/imagem.html')
+    fotografias = Fotografia.objects.all()
+    
+    return render(request, 'galeria/index.html', {"cards": fotografias})
+
+
+def imagem(request, foto_id):
+    fotografia = get_object_or_404(Fotografia, pk=foto_id)
+    return render(request, 'galeria/imagem.html', {"fotografia":fotografia})
